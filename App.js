5f8244c397dd79f1fb0186ae541792c23a7ac978
Dramos02 <ramosdominic0420@gmail.com>
2023-08-02 13:12:33 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+function App(){
+    return (
+        <div className="container">
+            <div className="grid">
+                <div className="dis"></div>
+                <div className="calcButton AC red">AC</div>
+                <div className="calcButton C red">C</div>
+                <div className="calcButton divide">/</div>
+                <div className="calcButton multipy">x</div>
+                <div className="calcButton seven">7</div>
+                <div className="calcButton eight">8</div>
+                <div className="calcButton nine">9</div>
+                <div className="calcButton subtract">-</div>
+                <div className="calcButton four">4</div>
+                <div className="calcButton five">5</div>
+                <div className="calcButton six">6</div>
+                <div className="calcButton add">+</div>
+                <div className="calcButton one">1</div>
+                <div className="calcButton two">2</div>
+                <div className="calcButton three">3</div>
+                <div className="calcButton equal green">=</div>
+                <div className="calcButton zero">0</div>
+                <div className="calcButton dot">.</div>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />,document.getElementById("root"));
